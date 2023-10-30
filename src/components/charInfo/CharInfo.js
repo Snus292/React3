509509bcf726da88c262a98ab34cfbedff8c3aa2
Snus292 <georgiigiblov@gmail.com>
2023-10-30 11:53:50 +0400

    Реализованно несколько компонентов

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,12 +1,12 @@
-import { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-import useMarvelService from '../../services/MarvelService';
-import Spinner from '../spinner/Spinner';
-import ErrorMessage from '../errorMessage/ErrorMessage';
-import Skeleton from '../skeleton/Skeleton';
+import useMarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import Skeleton from "../skeleton/Skeleton";
 
-import './charInfo.scss';
+import "./charInfo.scss";
 
 const CharInfo = (props) => {
 
@@ -51,9 +51,9 @@ const CharInfo = (props) => {
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki, comics} = char;
 
-    let imgStyle = {'objectFit' : 'cover'};
-    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-        imgStyle = {'objectFit' : 'contain'};
+    let imgStyle = {"objectFit" : "cover"};
+    if (thumbnail === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg") {
+        imgStyle = {"objectFit" : "contain"};
     }
 
     return (
@@ -77,7 +77,7 @@ const View = ({char}) => {
             </div>
             <div className="char__comics">Comics:</div>
             <ul className="char__comics-list">
-                {comics.length > 0 ? null : 'There is no comics with this character'}
+                {comics.length > 0 ? null : "There is no comics with this character"}
                 {
                     comics.map((item, i) => {
                         // eslint-disable-next-line
--- src/components/skeleton/Skeleton.js
+++ src/components/skeleton/Skeleton.js
@@ -1,4 +1,4 @@
-import './skeleton.scss';
+import "./skeleton.scss";
 
 const Skeleton = () => {
     return (
